ead49dec240ee494ac66f520230ed2931ce1e166
Arseniy SHISHAEV <ashishae@student.42.fr>
2021-02-24 16:02:55 +0100

    Tweak About link

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -4,12 +4,12 @@ class Nav extends React.Component {
 	render() {
 	return (
 			<div className="mt-10 flex items-center">
-				<div className="">
-					<h1 className="font-bold text-3xl">Preflight 🛫</h1>
-					<p className="text-gray-700">Share checklists with a click.</p>
+				<div className="flex-grow">
+					<h1 className="font-bold text-s md:text-3xl">Preflight 🛫</h1>
+					<p className="text-gray-700 text-xs md:text-xl">Share checklists with a click.</p>
 				</div>
-				<div className="mx-20 justify-self-end items-center">
-					<div className="font-bold text-gray-700 text-l">
+				<div className=" flex-grow-0 md:mx-20">
+					<div className="font-bold text-gray-700 md:text-3xl">
 						<h1>About</h1>
 					</div>
 				</div>
